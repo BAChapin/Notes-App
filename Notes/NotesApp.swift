3822c5b28036b5cb84c5d217387ee16006832847
Brett Chapin <brett.chapin89@gmail.com>
2024-06-12 14:41:15 -0400

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Brett Chapin on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
